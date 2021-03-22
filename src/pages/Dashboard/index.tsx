241f2b5a03838e7cb1e5d785250f6802753efe6a
Elton Santos <eltonaxl@hotmail.com>
2021-03-22 00:44:07 -0300

    Continuing in the course

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import SelectInput from '../../components/SelectInput';
+
+import ContentHeader from '../../components/ContentHeader';
+
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+
+  const options = [
+    {value: 'Elton', label: 'Elton'},
+    {value: 'Tamires', label: 'Tamires'},
+    {value: 'Hosana', label: 'Hosana'},
+  ]
+
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <ContentHeader title="Dashboard" lineColor="#F7931B">
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
   );
 }
 
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
-import { Container } from './styles';
+import SelectInput from '../../components/SelectInput';
+import HistoryFinanceCard from '../../components/HistoryFinanceCard';
+
+import ContentHeader from '../../components/ContentHeader';
+
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-  return  (
-    <h1>List</h1>
+
+  const months = [
+    {value: 7, label: 'Julho'},
+    {value: 8, label: 'Agosto'},
+    {value: 9, label: 'Setembro'},
+  ]
+
+  const years = [
+    {value: 2020, label: 2020},
+    {value: 2019, label: 2019},
+    {value: 2018, label: 2018},
+  ]
+
+  return (
+    <Container>
+      <ContentHeader title="Entradas" lineColor="#F7931B">
+        <SelectInput options={months} />
+        <SelectInput options={years} />
+      </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-eventual">Recorrentes</button>
+        <button type="button" className="tag-filter tag-filter-eventual">Eventuais</button>
+      </Filters>
+
+      <Content>
+        <HistoryFinanceCard
+          tagColor="#E44C4E"
+          title="Entradas"
+          subtitle="27/10/2020"
+          amount="200,00"
+        />
+      </Content>
+    </Container>
   );
 }
 
