c5b55e6a583070d901b3d5b1269cfd90c3d0a620
Elton Santos <eltonaxl@hotmail.com>
2021-03-28 03:18:59 -0300

    Finish change theme

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,22 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { Container, Profile, Welcome, UserName } from './styles';
 
 import Toggle from '../Toggle';
 import emojis from '../../utils/emojis';
 
+import { useTheme } from '../../hooks/theme';
+
 const MainHeader: React.FC = () => {
+
+  const { toggleTheme, theme } = useTheme();
+
+  const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
+
+  const handleChangeTheme = () => {
+      setDarkTheme(!darkTheme);
+      toggleTheme();
+  }
 
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length );
@@ -14,7 +25,7 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <Toggle />
+      <Toggle labelLeft="Light" labelRight="Dark" checked={darkTheme} onChange={handleChangeTheme} />
 
       <Profile>
         <Welcome>Olá, { emoji }</Welcome>
